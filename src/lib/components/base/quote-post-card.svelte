92012de5ed7e6c4ac5ede97d932d3f6bd071dc79
Harshit Walia <harshitwalia@github.com>
2022-12-26 20:01:23 +0530

    Revert "Quotes without date"
    This reverts commit b41daf8043e139fef8c761c8dbe5b735453ac4d1.

--- src/lib/components/base/quote-post-card.svelte
+++ src/lib/components/base/quote-post-card.svelte
@@ -1,14 +1,22 @@
 <script>
 	import Card from '$lib/components/base/card.svelte';
+	import Image from '$lib/components/base/image.svelte';
 	import Tag from './tag.svelte';
+	import dateformat from 'dateformat';
 	export let quote;
 	export let showImage = false;
 </script>
 
 {#if quote}
 	<Card>
+		<div slot="image">
+			{#if showImage}
+				<Image path="quotes/{quote.slug}" filename="cover" alt="Cover Image" />
+			{/if}
+		</div>
 		<div slot="content">
 			<div class="title">{quote.title}</div>
+			<div class="note">{dateformat(quote.date, 'UTC:dd mmmm yyyy')}</div>
 			<div class="text">{quote.excerpt}</div>
 		</div>
 		<div slot="footer">
