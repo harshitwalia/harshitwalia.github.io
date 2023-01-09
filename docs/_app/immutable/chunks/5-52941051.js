562b7a2e95e5afa336c5c3c071bc0fe4196f4e96
Harshit Walia <harshitwalia@github.com>
2023-01-09 23:52:26 +0530

    Coding Force

---
+++ docs/_app/immutable/chunks/5-52941051.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(root)/_page.svelte-d56744dd.js";const e=!0;export{t as component,e as server};
---
+++ docs/_app/immutable/chunks/9-734138ac.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(waves)/resume/_page.svelte-cd734372.js";export{t as component};
