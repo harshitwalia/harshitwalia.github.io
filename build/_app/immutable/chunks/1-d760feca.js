a891730bd3e429ce80f5b42c31926c5b4b16e2be
Harshit Walia <harshitwalia@github.com>
2024-05-15 10:22:57 +0530

    Updated Resume Link

--- build/_app/immutable/chunks/1-d760feca.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-1bc6d636.js";export{t as component};
--- build/_app/immutable/chunks/10-d19524aa.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/digital-tools/_page.md-3ca01423.js";export{t as component};
